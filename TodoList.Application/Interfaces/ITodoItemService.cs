9ec09f713df081e45b99ca24a27c19da2df2fed6
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2026-04-29 16:41:16 +0600

    Add ICategoryService.cs

--- TodoList.Application/Interfaces/ITodoItemService.cs
+++ TodoList.Application/Interfaces/ITodoItemService.cs
@@ -1,7 +1,12 @@
-﻿namespace TodoList.Application
-{
-    public class Class1
-    {
+﻿using TodoList.Application.DTOs;
 
-    }
+namespace TodoList.Application.Interfaces;
+
+public interface ICategoryService
+{
+    Task<List<CategoryDto>> GetAllAsync();
+    Task<CategoryDto?> GetByIdAsync(int id);
+    Task<CategoryDto> CreateAsync(string name, string color);
+    Task<CategoryDto?> UpdateAsync(int id, string name, string color);
+    Task<bool> DeleteAsync(int id);
 }
