eff0d46ea83b65489c34075a956d7bdcda7ff837
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2026-04-29 16:37:37 +0600

    Add TodoItemDto.cs

--- TodoList.Application/DTOs/TodoItemDto.cs
+++ TodoList.Application/DTOs/TodoItemDto.cs
@@ -1,10 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿namespace TodoList.Application.DTOs;
 
-namespace TodoList.Application.Entities
+public record TodoItemDto
 {
-    internal class Class1
-    {
-    }
+    public int Id { get; init; }
+    public string Title { get; init; } = string.Empty;
+    public string Description { get; init; } = string.Empty;
+    public bool IsCompleted { get; init; }
+    public int Priority { get; init; }
+    public DateTime DueDate { get; init; }
+    public DateTime CreatedAt { get; init; }
+    public DateTime UpdatedAt { get; init; }
+    public UserDto User { get; init; } = null!;
+    public CategoryDto? Category { get; init; }
+    public List<TagDto> Tags { get; init; } = new();
 }
