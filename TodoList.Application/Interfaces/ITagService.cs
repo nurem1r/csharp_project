7d4a7574b21c2222d906e203a9176e3932830615
nurem1r <nuremir.sulaimanov@alatoo.edu.kg>
2026-04-29 16:41:59 +0600

    Add ITagService.cs

---
+++ TodoList.Application/Interfaces/ITagService.cs
@@ -0,0 +1,12 @@
+﻿using TodoList.Application.DTOs;
+
+namespace TodoList.Application.Interfaces;
+
+public interface ITagService
+{
+    Task<List<TagDto>> GetAllAsync();
+    Task<TagDto?> GetByIdAsync(int id);
+    Task<TagDto> CreateAsync(string name);
+    Task<TagDto?> UpdateAsync(int id, string name);
+    Task<bool> DeleteAsync(int id);
+}
